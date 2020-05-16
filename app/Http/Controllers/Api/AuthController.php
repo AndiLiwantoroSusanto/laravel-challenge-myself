7b9475ffaf5bcbfdea6c8a948e68231c9d130627
BuildTools <unconfigured@null.spigotmc.org>
2020-05-16 13:54:07 +0700

    Push Composer for heroku

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -35,6 +35,6 @@ class AuthController extends Controller
         }
 
         $accessToken = Auth::user()->createToken('authToken')->accessToken;
-        return response(['user'=>auth()->user(),'access_token'=>$accessToken]);
+        return response(['user'=>Auth::user(),'access_token'=>$accessToken]);
     }
 }
