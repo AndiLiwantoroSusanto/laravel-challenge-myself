0bba47c49e851d91190fe1804612d6c3cde1df58
andiliewantorosusanto <[andiliesusanto@gmail.com]>
2020-08-12 23:18:33 +0700

    Test Commit

--- routes/api.php
+++ routes/api.php
@@ -35,6 +35,7 @@ Route::group(['prefix' => '/v1'], function ($api) {
             Route::post('/store', 'Api\v1\GoalController@store');
             Route::get('/index', 'Api\v1\GoalController@index');
             Route::post('/delete', 'Api\v1\GoalController@delete');
+            //asd
         });
         Route::post('/{id}/check-in', 'Api\v1\GoalController@checkIn');
     });
