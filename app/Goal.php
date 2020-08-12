9d44b39aa155305223748cf9a509081b79f09260
andiliewantorosusanto <andiliesusanto@gmail.com>
2020-08-13 00:11:50 +0700

    Softdelete

--- app/Goal.php
+++ app/Goal.php
@@ -3,11 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Goal extends Model
 {
+    use SoftDeletes;
     protected $fillable = [
         'name', 'achived_at', 'is_not_lazy','started_at','total_day','user_id'
     ];
 
+    protected $dates = ['deleted_at'];
 }
